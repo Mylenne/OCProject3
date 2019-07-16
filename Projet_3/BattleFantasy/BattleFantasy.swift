59e4238080da555b14554f1508c6970f487e3232
Mylenne <mylenne.tan@gmail.com>
2019-07-16 17:50:03 +0200

    fixed battle fantasy menu, added presentation func and fixed character type to be a constant

--- Projet_3/BattleFantasy/BattleFantasy.swift
+++ Projet_3/BattleFantasy/BattleFantasy.swift
@@ -9,31 +9,37 @@
 import Foundation
 
 class BattleFantasy {
-    static func run() {
+    // Battle Fantasy presentation
+    static func presentation() {
         print("Welcome to Battle Fantasy!! You will have to fight one against another team to win. Let's see who's the best! Good luck!😁😄")
         print("\n()=()   ()-()   ()=()   ()-()")
         print("('Y')   (':')   (^;^)   ('&')")
         print("q . p   d . b   C   C   c . c")
         print("()_()   ()_()   ()_()   ()=()\n")
+    }
+
+    // Battle Fantasy menu
+    static func menu() {
+        print("Please write \"Play\" to start a game or \"Quit\" to quit Battle Fantasy")
+        let choice = Utils.read()
         
-        func menu() {
-            print("Please write \"Play\" to start a game or \"Quit\" to quit Battle Fantasy")
-            let choice = Utils.read()
-            
-            switch choice {
-            case "Play":
-                let game = Game()
-                game.start()
-                print("Do you want to play again?")
-                menu()
-            case "Quit":
-                print("you quit the game 😥💩👎")
-            default:
-                print("You have to choose to play or to quit")
-                menu()
-            }
+        switch choice {
+        case "Play":
+            let game = Game()
+            game.start()
+            print("Do you want to play again?")
+            BattleFantasy.menu()
+        case "Quit":
+            print("You quit the game 😥💩👎")
+        default:
+            print("You have to choose to play or to quit")
+            BattleFantasy.menu()
         }
-        
-        menu()
+    }
+    
+    // Run Battle Fantasy
+    static func run() {
+        BattleFantasy.presentation()
+        BattleFantasy.menu()
     }
 }
